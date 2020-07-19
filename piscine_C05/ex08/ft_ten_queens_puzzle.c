4082ab21ac7d2aba10512dd9c5fb6a789db88f9c
Kyle <mkitigy@gmail.com>
2020-07-19 19:12:38 +0900

    edit C05

---
+++ piscine_C05/ex08/ft_ten_queens_puzzle.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/19 19:00:11 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/07/19 19:10:09 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void		print(char *str, int size)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < size)
+	{
+		c = *(str + i) + '0';
+		write(1, &c, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int			is_safe(char *places, int cur_row, int cur_col)
+{
+	int		i;
+
+	i = 0;
+	while (i < cur_row)
+	{
+		if (*(places + i) == cur_col)
+			return (-1);
+		if (*(places + i) > cur_col)
+		{
+			if ((cur_row - i) - (*(places + i) - cur_col) == 0)
+				return (-1);
+		}
+		else
+		{
+			if ((cur_row - i) - (cur_col - *(places + i)) == 0)
+				return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void		recursive_search(char *places, int cur_row, intptr_t *available)
+{
+	int		cur_col;
+
+	cur_col = 0;
+	if (cur_row == 10)
+	{
+		print(places, 10);
+		(*available)++;
+		return ;
+	}
+	while (cur_col < 10)
+	{
+		if (is_safe(places, cur_row, cur_col) == 0)
+		{
+			*(places + cur_row) = cur_col;
+			recursive_search(places, cur_row + 1, available);
+		}
+		cur_col++;
+	}
+	return ;
+}
+
+int			ft_ten_queens_puzzle(void)
+{
+	int		i;
+	long	available;
+	char	places[10];
+
+	available = 0;
+	i = 0;
+	while (i < 10)
+	{
+		*(places + i) = '.';
+		i++;
+	}
+	recursive_search(places, 0, &available);
+	return (available);
+}
+
+int			main(void)
+{
+	printf("%d", ft_ten_queens_puzzle());
+	return (0);
+}
