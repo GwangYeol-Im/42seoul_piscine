d01a325f992e5afade51dfba593f6cb27a72e30b
Kyle <mkitigy@gmail.com>
2020-07-19 19:11:59 +0900

    edit C08

--- piscine_C08/ex01/ft_boolean.h
+++ piscine_C08/ex01/ft_boolean.h
@@ -6,19 +6,22 @@
 /*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/17 21:46:17 by imgwang-yeo       #+#    #+#             */
-/*   Updated: 2020/07/17 21:53:55 by imgwang-yeo      ###   ########.fr       */
+/*   Updated: 2020/07/19 13:32:34 by imgwang-yeo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN
-# define FT_BOOLEAN
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
 #include <unistd.h>
 
 # define TRUE 1
 # define FALSE 0
-# define EVEN (x) (x % 2 == 0)
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+# define EVEN(num) num % 2 == 0 ? 1 : 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
+typedef char    t_bool;
 
 #endif
