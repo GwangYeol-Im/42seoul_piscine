d9fd0d447f8581c99df9a94975b75a580d5797c6
Kyle <mkitigy@gmail.com>
2020-07-20 10:57:49 +0900

    edit C08

---
+++ piscine_C08/ex05/ft_show_tab.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imgwang-yeol <imgwang-yeol@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/20 10:27:48 by imgwang-yeo       #+#    #+#             */
+/*   Updated: 2020/07/20 10:56:15 by imgwang-yeo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void		print_str(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+	write(1, "\n", 1);
+}
+
+void		recursive_print_int(int n)
+{
+	char	temp;
+
+	temp = n % 10 + '0';
+	if (n >= 10)
+		recursive_print_int(n / 10);
+	write(1, &temp, 1);
+}
+
+void		print_int(int n)
+{
+	recursive_print_int(n);
+	write(1, "\n", 1);
+}
+
+void		ft_show_tab(struct s_stock_str *par)
+{
+	int		i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		print_str(par[i].str);
+		print_int(par[i].size);
+		print_str(par[i].copy);
+		i++;
+	}
+}
